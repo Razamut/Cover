69e2bbf8e6e611024e6a9bfd3b7d3051f11fa2ef
Sannah Ziama <ziama@sannahs-mbp.attlocal.net>
2018-03-10 16:17:53 -0500

    added small improvements to CreateDatabases and LoanSummary modules

--- hrepo/CreateDatabases.hs
+++ hrepo/CreateDatabases.hs
@@ -1,33 +1,16 @@
 module CreateDatabases where
 
-import Text.CSV 
+import Text.CSV
 import Data.List
 import Data.Either
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
-
 {-
 -- drop last column in collections table and store as database table
-eitherCSV <- dropLastColumnInCSVFile "collections.csv"
 either (\err -> ["error"]) (\csv -> head csv) eitherCSV
-csv = either (\err -> [["error"]]) (\csv -> csv) eitherCSV
-convertCSVtoSQL "collections" "collections.sql" ["id INTEGER","loan_id TEXT","loan_plus_intr_usd REAL","loan_plus_intr_ld REAL","payment_dt TEXT","payment_amt_usd REAL","payment_amt_ld REAL"] csv
-
--- save the loans table in database
-convertCSVFiletoSQL "loans.csv" "loans.sql" "loans" ["id INTEGER","loan_id TEXT","loan_type TEXT","amt_requested TEXT","loan_amt_usd REAL","loan_amt_ld REAL","rate REAL","interest_ld REAL","interest_usd REAL","duration_month REAL","status TEXT","take_out_dt TEXT","loan_plus_intr_ld REAL","loan_plus_intr_usd REAL"]
-
---save the connections table in database
-convertCSVFiletoSQL "connections.csv" "connections.sql" "connections" ["id INTEGER","employment TEXT","employer_name TEXT","occupation TEXT","monthly_salary TEXT","mobile_type TEXT","marital_status TEXT","spouse_first_name TEXT","spouse_last_name TEXT","spouse_age TEXT","spouse_employement_status TEXT","home_address TEXT","business_address TEXT","employer_address TEXT","business_description TEXT","fb_acct TEXT","housing_type TEXT","rent_amt REAL","family_size TEXT","n_kids INTEGER","kids_sch_type TEXT","spouse_employed TEXT"]
-
 queryDatabase "connections.sql" "SELECT COUNT(*) FROM connections WHERE spouse_age > 24;"
-
---save the person table into database
-convertCSVFiletoSQL "person.csv" "person.sql" "person" ["id INTEGER","first_name TEXT","last_name TEXT","middle_name TEXT","id_number TEXT","id_type TEXT","age REAL","gender TEXT","ph_numbers REAL","email TEXT"]
-
 -}
-
-
 fieldsInCSVFile :: FilePath -> IO ([String])
 fieldsInCSVFile inFileName = do
   input <- readFile inFileName
@@ -35,7 +18,6 @@ fieldsInCSVFile inFileName = do
   case records of
     Left error -> return $ ["parse error"]
     Right csv  -> return $ head csv
-
 
 dropLastColumnInCSV :: CSV -> CSV
 dropLastColumnInCSV csv = map (take n) csv
@@ -89,10 +71,20 @@ convertCSVtoSQL tableName outFileName fields records =
                       intercalate ", " (replicate nfieldsInFile "?") ++ ")"
     sqlRecords = map (map toSql) records
 
-
 queryDatabase :: FilePath -> String -> IO [[SqlValue]]
 queryDatabase databaseFile sqlQuery = do
   conn <- connectSqlite3 databaseFile
   result <- quickQuery' conn sqlQuery []
   disconnect conn
   return result
+
+main :: IO ()
+main = do
+  eitherCSV <- dropLastColumnInCSVFile "collections.csv"
+  let csv = either (\err -> [["error"]]) (\csv -> csv) eitherCSV
+  convertCSVtoSQL "collections" "collections.sql" ["id INTEGER","loan_id TEXT","loan_plus_intr_usd REAL","loan_plus_intr_ld REAL","payment_dt TEXT","payment_amt_usd REAL","payment_amt_ld REAL"] csv
+  convertCSVFiletoSQL "loans.csv" "loans.sql" "loans" ["id INTEGER","loan_id TEXT","loan_type TEXT","amt_requested TEXT","loan_amt_usd REAL","loan_amt_ld REAL","rate REAL","interest_ld REAL","interest_usd REAL","duration_month REAL","status TEXT","take_out_dt TEXT","loan_plus_intr_ld REAL","loan_plus_intr_usd REAL"]
+  convertCSVFiletoSQL "connections.csv" "connections.sql" "connections"
+    ["id INTEGER","employment TEXT","employer_name TEXT","occupation TEXT","monthly_salary TEXT","mobile_type TEXT","marital_status TEXT","spouse_first_name TEXT","spouse_last_name TEXT","spouse_age TEXT","spouse_employement_status TEXT","home_address TEXT","business_address TEXT","employer_address TEXT","business_description TEXT","fb_acct TEXT","housing_type TEXT","rent_amt REAL","family_size TEXT","n_kids INTEGER","kids_sch_type TEXT","spouse_employed TEXT"]
+  convertCSVFiletoSQL "person.csv" "person.sql" "person" ["id INTEGER","first_name TEXT","last_name TEXT","middle_name TEXT","id_number TEXT","id_type TEXT","age REAL","gender TEXT","ph_numbers REAL","email TEXT"]
+  putStrLn "Successfully created all databases!"
--- hrepo/LoanSummary.hs
+++ hrepo/LoanSummary.hs
@@ -1,63 +1,22 @@
 module LoanSummary where
 
 import Data.List
+import Data.Char (toLower)
 import Text.CSV
 import Data.Either
 import Database.HDBC
 import Database.HDBC.Sqlite3
-import CreateDatabases
+import CreateDatabases (queryDatabase)
 
 {-
 --loans schema
 ["id INTEGER","loan_id TEXT","loan_type TEXT","amt_requested TEXT","loan_amt_usd REAL","loan_amt_ld REAL","rate REAL","interest_ld REAL","interest_usd REAL","duration_month REAL","status TEXT","take_out_dt TEXT","loan_plus_intr_ld REAL","loan_plus_intr_usd REAL"]
 --collections schema
 ["id INTEGER","loan_id TEXT","loan_plus_intr_usd REAL","loan_plus_intr_ld REAL","payment_dt TEXT","payment_amt_usd REAL","payment_amt_ld REAL"]
-
-
-sqlLDloanRecords <- queryDatabase "loans.sql" "SELECT loan_type, rate, loan_amt_ld FROM loans WHERE status = 'approved' "
-ldLoanRecords = zip3 (readStringColumn sqlLDloanRecords 0) (readDoubleColumn sqlLDloanRecords 1) (readDoubleColumn sqlLDloanRecords 2)
-ldLoanTypeAmt = map (\(a, _, c) -> (a, c)) ldLoanRecords
-ldTotalLoans = sum $ map (\(_, b) -> b) ldLoanTypeAmt
-ldTotalPersonalLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> a == "p") ldLoanTypeAmt
-ldTotalBusinessLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> a == "b") ldLoanTypeAmt
-
-ldLoanTypeExpectedCollections = map (\(a, b, c) -> (a, c + b / 100.0 * c)) ldLoanRecords
-ldTotalExpectedCollections = sum $ map (\(_, b) -> b) ldLoanTypeExpectedCollections
-
-sqlUSDloanRecords <- queryDatabase "loans.sql" "SELECT loan_type, rate, loan_amt_usd FROM loans WHERE status = 'approved' "
-usdLoanRecords = zip3 (readStringColumn sqlUSDloanRecords 0) (readDoubleColumn sqlUSDloanRecords 1) (readDoubleColumn sqlUSDloanRecords 2)
-usdLoanTypeAmt = map (\(a, _, c) -> (a, c)) usdLoanRecords
-usdTotalLoans = sum $ map (\(_, b) -> b) usdLoanTypeAmt
-usdTotalPersonalLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> a == "p") usdLoanTypeAmt
-usdTotalBusinessLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> a == "b") usdLoanTypeAmt
-
-usdLoanTypeExpectedCollections = map (\(a, b, c) -> (a, c + b / 100.0 * c)) usdLoanRecords
-usdTotalExpectedCollections = sum $ map (\(_, b) -> b) usdLoanTypeExpectedCollections
-
-xchangeRate  = 115 :: Double
-totalExpectedCollections = ldTotalExpectedCollections / xchangeRate + usdTotalExpectedCollections
-
-totalDisbursedLoans = ldTotalLoans / xchangeRate + usdTotalLoans
-totalPersonalLoans = ldTotalPersonalLoans / xchangeRate + usdTotalPersonalLoans
-totalBusinessLoans = ldTotalBusinessLoans / xchangeRate + usdTotalBusinessLoans
-
-
-sqlCollectionRecords <- queryDatabase "collections.sql" "SELECT payment_amt_ld, payment_amt_usd FROM collections"
-collectionRecords = zip (readDoubleColumn sqlCollectionRecords 0) (readDoubleColumn sqlCollectionRecords 1)
-ldTotalCollections = sum $ map (\(a,_) -> a) collectionRecords
-usdTotalCollections = sum $ map (\(_, b) -> b) collectionRecords
-totalCollection = ldTotalCollections / xchangeRate + usdTotalCollections
-
-totalOutstanding = totalExpectedCollections - totalCollection
-
 plotList [PNG "gnu_line.png", Title "line", YRange (0.0, 20.0)] (zip [1..] values)
-
-
-
 -}
 
 --read the sqlValues from SQLite3 DB and convert to Haskell values
-
 readIntegerColumn :: [[SqlValue]]  -> Integer -> [Integer]
 readIntegerColumn sqlResult index =
   map (\row -> fromSql $ genericIndex row index :: Integer) sqlResult
@@ -74,4 +33,106 @@ safeConvertToDouble :: SqlValue -> Double
 safeConvertToDouble value =
   case value of
     SqlDouble x  ->  x
-    _                    ->  0.0
+    _            ->  0.0
+
+getLoanRecordsFromQuery :: String -> String -> IO [(String, Double, Double)]
+getLoanRecordsFromQuery dbName query = do
+  sqlLoanRecords <- queryDatabase dbName query
+  let loanRecords = zip3 (readStringColumn sqlLoanRecords 0) (readDoubleColumn sqlLoanRecords 1) (readDoubleColumn sqlLoanRecords 2)
+  return loanRecords
+
+getLoanRecords :: String -> String -> IO ( Either String [(String, Double, Double)] )
+getLoanRecords dbName loanType = case map toLower loanType of
+  "ld" -> do
+            let query = "SELECT loan_type, rate, loan_amt_ld FROM loans WHERE status = 'approved' "
+            records <- getLoanRecordsFromQuery dbName query
+            return $ Right records
+  "usd" -> do
+             let query = "SELECT loan_type, rate, loan_amt_usd FROM loans WHERE status = 'approved' "
+             records <- getLoanRecordsFromQuery dbName query
+             return $ Right records
+  _   -> do
+           return $ Left "Provide a valid loan type. Valid loan types are LD or USD."
+
+getLoansDisbursed' :: [(String, Double, Double)] -> (Double, Double, Double, Double)
+getLoansDisbursed' records = (totalLoans, totalPersonalLoans, totalBusinessLoans, totalOtherLoans)
+  where
+    loanTypeAmt = map (\(a, _, c) -> (a, c)) records
+    totalLoans = sum $ map (\(_, b) -> b) loanTypeAmt
+    totalPersonalLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> a == "p") loanTypeAmt
+    totalBusinessLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> a == "b") loanTypeAmt
+    totalOtherLoans = sum $ map (\(_, b) -> b) $ filter (\(a, b) -> and [a /= "p", a/="b"] ) loanTypeAmt
+
+getLoansDisbursed :: String -> IO ()
+getLoansDisbursed loanType = do
+  loanRecords <- getLoanRecords "loans.sql" loanType
+  case loanRecords of
+    Right records -> do
+      let disbursedRecord = getLoansDisbursed' records
+      putStrLn $ "total loans disbursed = " ++ show ((\(a,_,_,_) -> a) disbursedRecord)
+      putStrLn $ "personal loans disbursed = " ++ show ((\(_,b,_,_) -> b) disbursedRecord)
+      putStrLn $ "business loans disbursed = " ++ show ((\(_,_,c,_) -> c) disbursedRecord)
+      putStrLn $ "other loans disbursed = " ++ show ((\(_,_,_,d) -> d) disbursedRecord)
+    Left err -> putStrLn err
+
+getTotalDisbursed :: Double -> IO Double
+getTotalDisbursed exchangeRate = do
+  ldLoanRecords <- getLoanRecords "loans.sql" "LD"
+  usdLoanRecords <- getLoanRecords "loans.sql" "USD"
+  let usdTotalDisbursed = (\(a,_,_,_) -> a) $ getLoansDisbursed' $ (\(Right x) -> x) usdLoanRecords
+  let ldTotalDisbursed = (\(a,_,_,_) -> a) $ getLoansDisbursed' $ (\(Right x) -> x) ldLoanRecords
+  return $ ldTotalDisbursed / exchangeRate + usdTotalDisbursed
+
+getExpectedCollection' :: [(String, Double, Double)] -> Double
+getExpectedCollection' records = totalExpectedCollections
+  where
+    loanTypeExpectedCollections = map (\(a, b, c) -> (a, c + b / 100.0 * c)) records
+    totalExpectedCollections = sum $ map (\(_, b) -> b) loanTypeExpectedCollections
+
+getExpectedCollection :: String -> IO ()
+getExpectedCollection loanType = do
+  loanRecords <- getLoanRecords "loans.sql" loanType
+  case loanRecords of
+    Right records -> putStrLn $ "expected collection = " ++ show (getExpectedCollection' records)
+    Left err -> putStrLn err
+
+getTotalExpectedCollection :: Double -> IO Double
+getTotalExpectedCollection exchangeRate = do
+  ldLoanRecords <- getLoanRecords "loans.sql" "LD"
+  usdLoanRecords <- getLoanRecords "loans.sql" "USD"
+  let ldExpectedCollection = getExpectedCollection' $ (\(Right x) -> x) ldLoanRecords
+  let usdExpectedCollection = getExpectedCollection' $ (\(Right x) -> x) usdLoanRecords
+  let totalExpected = ldExpectedCollection / exchangeRate + usdExpectedCollection
+  return totalExpected
+
+getTotalCollectionRecords :: Double -> IO (Double, Double, Double)
+getTotalCollectionRecords xchangeRate = do
+  sqlCollectionRecords <- queryDatabase "collections.sql" "SELECT payment_amt_ld, payment_amt_usd FROM collections"
+  let collectionRecords = zip (readDoubleColumn sqlCollectionRecords 0) (readDoubleColumn sqlCollectionRecords 1)
+  let ldTotalCollections = sum $ map (\(a,_) -> a) collectionRecords
+  let usdTotalCollections = sum $ map (\(_, b) -> b) collectionRecords
+  let totalCollection = ldTotalCollections / xchangeRate + usdTotalCollections
+  return (totalCollection, ldTotalCollections, usdTotalCollections)
+
+main :: IO ()
+main = do
+  putStrLn "LD PORTFOLIO :"
+  getLoansDisbursed "LD"
+  putStrLn "USD PORTFOLIO :"
+  getLoansDisbursed "USD"
+  totalDisbursed <- getTotalDisbursed 115.0
+  putStrLn $ "Total (LD + USD) Disbursed Loans = " ++ show totalDisbursed
+  putStrLn "LD Expected Collections:"
+  getExpectedCollection "LD"
+  putStrLn "USD Expected Collections:"
+  getExpectedCollection "USD"
+  totalExpected <- getTotalExpectedCollection 115.0
+  putStrLn $ "total expected collection = " ++ show totalExpected
+  collectionRecord <- getTotalCollectionRecords 115.0
+  putStrLn $ "total collection = " ++ show ((\(a,_,_) -> a) collectionRecord)
+  putStrLn $ "LD collections = " ++ show ((\(_,b,_) -> b) collectionRecord)
+  putStrLn $ "USD collections = " ++ show ((\(_,_,c) -> c) collectionRecord)
+  putStrLn "total outstanding : "
+  let totalCollection = ((\(a,_,_) -> a) collectionRecord)
+  let totalOutstanding = totalExpected - totalCollection
+  print $ totalOutstanding
